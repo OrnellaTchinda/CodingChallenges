bf4015960abaa9c6c69f909ab9ce85218a5141a2
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2020-10-24 15:54:57 -0400

    Added slt to my CC

--- CC-2022-Oct-21/app.js
+++ CC-2022-Oct-21/app.js
@@ -11,7 +11,7 @@
 //P: integer
 //R: day or string
 //E: 1 returns "Sunday" ; 2 returns "Monday" ; 3 returns "Tuesday"
-//P : Use the switch loop to connect each number to the day or create an object and return
+//P : Use the switch loop to connect each number to the day or create an object and return the object[num]
 
 function whatday(num) {
     switch (num) {
