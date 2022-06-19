ebd3b4178a085f698c4b56eeb2fdfc28aadaac1e
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-06-18 20:46:01 -0400

    Added Parameters, return, examples, pseudoCode to june 16th,2022 Coding challenge

--- CC-16-June-2022/app.js
+++ CC-16-June-2022/app.js
@@ -8,6 +8,17 @@ function repeatStr(n, s) {
     return s.repeat(n); //The repeat method return a string with a number of copis of a string without spaces
 }
 
+function repeatStr(n, s) {
+    //Params: a number and a string
+    //Returns: a string with the initial string repeated n times
+    //Examples: repeatStr(3, "*"), "***"); repeatStr(5, "#"), "#####"); repeatStr(2, "ha "), "ha ha ");
+    //Pseudocode: initialize new string, loop from 0 to n, every loop add the value of the initial string to the new string. return new string
+    let newStr = ""
+    for (let i = 0; i < n; i++) {
+        newStr += s
+    }
+    return newStr;
+}
 
 //Try using the for loop I know you don't always like it
 
