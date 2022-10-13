88baea32397691af054acbccb5ab231cc204f11e
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-10-13 14:06:49 -0400

    Added one line slt to Oct12 CC

--- CC-2022-Oct-12/app.js
+++ CC-2022-Oct-12/app.js
@@ -36,5 +36,8 @@ function sum(numbers) {
         return numbers.reduce((a, b) => a + b, 0)
     }
 
+};
 
+function sum(numbers) {
+    return (numbers.length === 0 ? 0 : numbers.reduce((a, b) => a + b, 0));
 };
