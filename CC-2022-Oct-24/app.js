cae8e328cb782654049ec485663a1c060619dad7
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-10-14 23:32:08 -0400

    Added slt to Oct 24th CC

---
+++ CC-2022-Oct-24/app.js
@@ -0,0 +1,12 @@
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+// Graph square with an arc from points in diagonal and le rayon un cote du carre
+
+// Note: use the π value provided in your language (Math::PI, M_PI, math.pi, etc)
+
+function squareArea(A) {
+
+    let r = (A * 4) / (2 * Math.PI)  //Circumference formula =2*pi*r and those arcs form a circle when 4 of them are added
+
+    return Math.pow(r, 2)
+}
