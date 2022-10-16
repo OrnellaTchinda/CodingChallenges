88fae6132a0f1884912b2ad6dd017a0858425ff7
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-10-15 23:34:32 -0400

    Added slt for Oct 24th CC

--- CC-2022-Oct-24/app.js
+++ CC-2022-Oct-24/app.js
@@ -10,3 +10,17 @@ function squareArea(A) {
 
     return Math.pow(r, 2)
 }
+
+function squareArea(A) {
+    // Calculate the circumference of A 
+    const circumference = A * 4;
+
+    // Calculate the radius of A
+    const radius = circumference / (Math.PI * 2);
+
+    // Calculate the area of the square, rounded to 2 decimal places
+    const area = Number((radius ** 2).toFixed(2));
+
+    // Return the result
+    return area;
+}
