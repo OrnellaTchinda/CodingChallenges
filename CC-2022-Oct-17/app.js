c9279ec114c8af86054bd3833d9688cccf1230df
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-10-17 19:01:54 -0400

    Added slt to Oct 17th CC

---
+++ CC-2022-Oct-17/app.js
@@ -0,0 +1,12 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//P: An integer
+//R: An integer
+//E: doubleInteger(2),4
+//P: times 2 
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+
+    return i * 2;
+}
