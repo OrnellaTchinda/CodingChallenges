0afb506b4d143de4a3a365595606ae988f25fe84
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-10-19 21:00:29 -0400

    Added slt for Oct 19th CC

--- CC-2022-Oct-19/app.js
+++ CC-2022-Oct-19/app.js
@@ -30,3 +30,11 @@ function numberToPower(number, power) {
     }
 
 }
+
+function numberToPower(number, power) {
+    let total = 1;
+    for (let i = 1; i <= power; i++) {
+        total = total * number;
+    }
+    return total;
+}
