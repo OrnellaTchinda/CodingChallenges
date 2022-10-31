f1d91e0c59dac59f2a916a2920e2f18fc2468c74
Ornella <ornella.tchinda@yahoo.fr>
2022-10-31 00:37:14 -0400

    Added slt to string array duplicate kata

---
+++ CC-2022-Oct-30/app.js
@@ -0,0 +1,19 @@
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+function dup(s) {
+    //array of strings
+    //array of string with non consecutive/duplicate letters
+    //lowercase
+    let newArr = []
+    return s.forEach(x => newArr.push((x.split('').filter((w, i, a) => w != a[i - 1]).join(''))))
+
+
+};
