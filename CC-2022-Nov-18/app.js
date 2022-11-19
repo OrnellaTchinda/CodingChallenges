d1bfa8cecc3e40a7943c3027f4553ece83391595
Ornella <ornella.tchinda@yahoo.fr>
2022-11-18 23:02:26 -0500

    Added slt to Nov 18th CC

---
+++ CC-2022-Nov-18/app.js
@@ -0,0 +1,9 @@
+// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+function angle(n) {
+
+    //sum of internal angles formula = (n-2)*180
+    
+    return ((n-2)*180)
+    
+  }
