092bdadbfaccda30a1f78ad0c4271127bb1f3945
Ornella <ornella.tchinda@yahoo.fr>
2022-12-02 22:00:13 -0500

    Slt to reverse Str no reverse method

---
+++ CC-2022-Dec-04/app.js
@@ -0,0 +1,11 @@
+// Given a string of characters as input, write a function that returns it with the characters reversed. No Reverse Method (well brute force it first, but then no reverse method)! 
+
+function reverseStr(str) {
+    let newStr = [];
+    let splitedStr = str.split('');
+    for (let i = splitedStr.length - 1; i > 0; i--) {
+        newStr.push([splitedStr[i]]);
+    }
+
+    return newStr.join('');
+}
