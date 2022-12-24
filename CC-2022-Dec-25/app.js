0ceb690de7908888378b9a2b3dcd656e1fb7e23a
Ornella <ornella.tchinda@yahoo.fr>
2022-12-24 16:17:39 -0500

    Leetcode 2nd slt to problem 540

--- CC-2022-Dec-25/app.js
+++ CC-2022-Dec-25/app.js
@@ -22,3 +22,18 @@ var singleNonDuplicate = function (nums) {
     return nums[nums.length - 1]
 
 };
+
+var singleNonDuplicate = function (nums) {
+    //easy solution
+
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1] || nums[i] === nums[i + 1]) {
+            continue
+        } else {
+            return nums[i]
+        }
+
+    }
+
+};
