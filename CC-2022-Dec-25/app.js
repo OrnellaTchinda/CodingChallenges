9988462459913feaa087744b56f4107da9ab7283
Ornella <ornella.tchinda@yahoo.fr>
2022-12-25 15:07:08 -0500

    Leetcode slt to problem 540

---
+++ CC-2022-Dec-25/app.js
@@ -0,0 +1,24 @@
+// You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+
+// Return the single element that appears only once.
+
+// Your solution must run in O(log n) time and O(1) space.
+
+var singleNonDuplicate = function (nums) {
+    //check if the element before and after is the same if yes continue, else return the element
+
+    if (nums[0] !== nums[1]) {
+        return nums[0];
+    }
+
+    for (let i = 1; i < nums.length - 1; i++) {
+        if (nums[i] === nums[0] || nums[i] === nums[i + 1]) {
+            continue
+        } else {
+            return nums[i]
+        }
+    }
+
+    return nums[nums.length - 1]
+
+};
