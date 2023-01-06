cde15474cf9b7af82eafd6e330ca736f4597c1f4
Ornella <ornella.tchinda@yahoo.fr>
2023-01-06 07:16:38 -0500

    Added slt to TwoSum leetcode pb

---
+++ CC-2023-Jan-07/TwoSum.js
@@ -0,0 +1,46 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+//P: array an integer;
+//R: array and integer
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+//Create a map object;
+//Add the first value of the array in the map object as a key and the index in which it is as the value
+//Substract each element with the target, get check if it's the object. If not add it with its index as value.
+
+
+var twoSum = function (nums, target) {
+    const map2 = new Map();
+    map2.set(nums[0], 0);
+
+
+    for (let i = 1; i < nums.length; i++) {
+
+
+        const result = target - nums[i];
+
+        if (map2.has(result)) {
+            return [map2.get(result), i]
+        } else {
+            map2.set(nums[i], i)
+        }
+
+    }
+}
